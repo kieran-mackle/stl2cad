9ad0cad2f9e288544bfcb94f436d93322ea8ba5e
kieran-mackle <kemackle98@gmail.com>
2024-12-05 17:24:55 +1000

    docs: provided example install for freecad
    also removed local path references

--- src/stl2cad/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# TODO - this needs to be generalised
-sys.path.insert(0, "/home/kieran/Downloads/freecad_appimage/squashfs-root/usr/lib")
-sys.path.insert(
-    0,
-    "/home/kieran/Downloads/freecad_appimage/squashfs-root/usr/lib/python3.11/site-packages",
-)
